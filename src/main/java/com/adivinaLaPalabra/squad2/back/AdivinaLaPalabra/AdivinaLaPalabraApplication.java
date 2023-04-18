5a52c21bd67c8d00f2211d8260f230fbedecfd8d
paufandos-soprasteria <pau.fandos@ext.soprasteria.com>
2023-04-18 09:13:54 +0200

    build(#15):Crear proyecto Java + instalar dependencias

---
+++ src/main/java/com/adivinaLaPalabra/squad2/back/AdivinaLaPalabra/AdivinaLaPalabraApplication.java
@@ -0,0 +1,13 @@
+package com.adivinaLaPalabra.squad2.back.AdivinaLaPalabra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdivinaLaPalabraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdivinaLaPalabraApplication.class, args);
+	}
+
+}

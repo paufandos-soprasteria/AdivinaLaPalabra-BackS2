a0246c4a37300e67ae2599539c0b557135f24e60
paufandos-soprasteria <pau.fandos@ext.soprasteria.com>
2023-04-19 16:27:50 +0200

    feat(#22): gestion de errores

--- src/main/java/com/adivinaLaPalabra/squad2/back/AdivinaLaPalabra/controllers/DictionaryController.java
+++ src/main/java/com/adivinaLaPalabra/squad2/back/AdivinaLaPalabra/controllers/DictionaryController.java
@@ -1,10 +1,14 @@
 package com.adivinaLaPalabra.squad2.back.AdivinaLaPalabra.controllers;
 
 import com.adivinaLaPalabra.squad2.back.AdivinaLaPalabra.services.impl.DictionaryServiceImpl;
+
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class DictionaryController {
     @Autowired
     public DictionaryServiceImpl dictionaryService;
+
+    @ExceptionHandler({ SQLException.class, Exception.class })
+    public ResponseEntity<String> handleDatabaseExceptions(Exception e) {
+        System.out.println("ERROR - " + e.getMessage());
+        return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     @GetMapping("/checkIfWordExists/{word}")
     ResponseEntity<?> checkIfWordExists(@PathVariable("word") String word) {
--- src/test/java/com/adivinaLaPalabra/squad2/back/AdivinaLaPalabra/services/impl/GameServiceImplTest.java
+++ src/test/java/com/adivinaLaPalabra/squad2/back/AdivinaLaPalabra/services/impl/GameServiceImplTest.java
@@ -2,8 +2,6 @@ package com.adivinaLaPalabra.squad2.back.AdivinaLaPalabra.services.impl;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @ExtendWith(MockitoExtension.class)
 public class GameServiceImplTest {

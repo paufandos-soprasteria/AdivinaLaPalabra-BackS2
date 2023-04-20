a8409051f438729baeac732b77f7484d621493d8
paufandos-soprasteria <pau.fandos@ext.soprasteria.com>
2023-04-20 12:31:01 +0200

    refactor(#22): gestion de errores

--- src/main/java/com/adivinaLaPalabra/squad2/back/AdivinaLaPalabra/services/impl/GameServiceImpl.java
+++ src/main/java/com/adivinaLaPalabra/squad2/back/AdivinaLaPalabra/services/impl/GameServiceImpl.java
@@ -5,7 +5,6 @@ import com.adivinaLaPalabra.squad2.back.AdivinaLaPalabra.entities.Game;
 import com.adivinaLaPalabra.squad2.back.AdivinaLaPalabra.repositories.DictionaryRepository;
 import com.adivinaLaPalabra.squad2.back.AdivinaLaPalabra.repositories.GameRepository;
 import com.adivinaLaPalabra.squad2.back.AdivinaLaPalabra.utilities.Utils;
-import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,24 +15,26 @@ public class GameServiceImpl {
     @Autowired
     public DictionaryRepository dictionaryRepository;
 
-    static final Utils UTILS = new Utils();
-    public int newGame(){
-        String datenow = UTILS.generateDateNowEspFormat();
+    public int newGame() {
+        String datenow = Utils.generateDateNowEspFormat();
         int dictionarySize = getDictionarySize();
-        int idWord = UTILS.generateRandomNumberInRange(dictionarySize);
+        int idWord = Utils.generateRandomNumberInRange(dictionarySize);
         Dictionary dictionaryWord = getDictionaryColumn(idWord);
-        Game newGame = new Game(dictionaryWord.getWord(),datenow);
+        Game newGame = new Game(dictionaryWord.getWord(), datenow);
         saveNewGame(newGame);
-        System.out.println("Partida número "+newGame.id);
+        System.out.println("Partida número " + newGame.id);
         return newGame.getId();
     }
+
     private void saveNewGame(Game newGame) {
         gameRepository.save(newGame);
     }
+
     private int getDictionarySize() {
         return (int) dictionaryRepository.count();
     }
-    private Dictionary getDictionaryColumn(int idWord){
+
+    private Dictionary getDictionaryColumn(int idWord) {
         return dictionaryRepository.getReferenceById(idWord);
     }
 }
--- src/main/java/com/adivinaLaPalabra/squad2/back/AdivinaLaPalabra/utilities/Utils.java
+++ src/main/java/com/adivinaLaPalabra/squad2/back/AdivinaLaPalabra/utilities/Utils.java
@@ -3,15 +3,27 @@ package com.adivinaLaPalabra.squad2.back.AdivinaLaPalabra.utilities;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import org.json.simple.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class Utils {
     static final DateTimeFormatter ESP_DATE_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    public String generateDateNowEspFormat(){
+    public static String generateDateNowEspFormat(){
         LocalDate now = LocalDate.now();
         String datenow = ESP_DATE_FORMAT.format(now);
         return datenow;
     }
-    public int generateRandomNumberInRange(int range){
+    public static int generateRandomNumberInRange(int range){
        return (int) (Math.random() * range) +1;
     }
+
+    public static ResponseEntity<JSONObject> errorResponse(HttpStatus status) {
+        JSONObject response = new JSONObject();
+        response.put("status", status.value());
+        response.put("message", status);
+
+        return new ResponseEntity<JSONObject>(response, status);
+    } 
 }

b66333a6f9f10a6434cf8819ebda0181967c465f
paufandos-soprasteria <130562716+paufandos-soprasteria@users.noreply.github.com>
2023-04-20 13:09:07 +0200

    refactor(#24): crear endpoint nueva partida (#10)

--- src/main/java/com/adivinaLaPalabra/squad2/back/AdivinaLaPalabra/controllers/GameController.java
+++ src/main/java/com/adivinaLaPalabra/squad2/back/AdivinaLaPalabra/controllers/GameController.java
@@ -1,6 +1,8 @@
 package com.adivinaLaPalabra.squad2.back.AdivinaLaPalabra.controllers;
 
 import com.adivinaLaPalabra.squad2.back.AdivinaLaPalabra.services.impl.GameServiceImpl;
+
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,10 +11,11 @@ import org.springframework.web.bind.annotation.*;
 public class GameController {
     @Autowired
     public GameServiceImpl gameService;
+
     @GetMapping("/newGame")
-    public int newGame()
-    {
-        return gameService.newGame();
+    public JSONObject newGame() {
+        JSONObject response = new JSONObject();
+        response.put("game_id", gameService.newGame());
+        return response;
     }
 }
-
